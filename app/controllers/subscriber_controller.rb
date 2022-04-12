245436057435d631293efcaf50c6066d3c337f3c
Toby Manley <tobymanley1990@gmail.com>
2022-04-12 21:58:14 +0900

    Added routes and built out show and create in controller

--- app/controllers/subscriber_controller.rb
+++ app/controllers/subscriber_controller.rb
@@ -3,11 +3,19 @@ class SubscriberController < ApplicationController
   before_action :set_referrer, only: [:create, :confirm]
 
   def show
-
+    render json: @subscriber, :only => ['email', 'user_key', 'share_key', 'sent_to_airtable', 'prize_sent'], :include => {:referrals => { :only => :email } }
   end
 
   def create
+    @subscriber = Subscriber.new(subscriber_params)
 
+    if @subscriber.save
+      if @referrer
+        # send email here to confirm the referral eamils
+      end
+
+      render json: @subscriber.errors, status: :unprocessable_entity
+    end
   end
 
   def confirm
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/subscriber/:id' => 'subscriber#show'
+  post '/confirm-subscriber/:id' => 'subscriber#confirm'
+  post '/subscriber' => 'subscriber#create'
+  put '/subscriber/:id' => 'subscriber#update'
 end
